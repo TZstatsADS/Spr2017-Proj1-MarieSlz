e75e2ccc7559568e65524eacf9aee9a7fce0129d
MarieSlz <marie.c.schiltz@gmail.com>
2017-02-02 23:50:21 -0500

    Commit

---
+++ lib/displaying.R
@@ -0,0 +1,56 @@
+
+
+wordcloud.comparison <- function (arg1, arg2) {
+  # Worcloud
+  term1 <- paste0(arg2,"-1")
+  term2 <- paste0(arg2,"-2")
+  selected.data1 <- arg1[arg1$document==term1,]
+  selected.data2 <- arg1[arg1$document==term2,]
+  
+  layout(matrix(c(1,3,2,4), nrow=2, ncol=2), heights=c(0.5, 3))
+  #layout.show(4)
+  par(mar=c(1,0,0,0)) #margins
+  plot.new()
+  text(x=0.5, y=0.5, cex=2, "1st Term", font=4)
+  plot.new()
+  text(x=0.5, y=0.5, cex=2, "2nd Term", font=4)
+  wordcloud(selected.data1$word, selected.data1$count,
+            main = "1st Term",
+            scale=c(5,0.5),
+            max.words=70,
+            min.freq=1,
+            random.order=FALSE,
+            rot.per=0.0,
+            use.r.layout=T,
+            random.color=FALSE,
+            colors=brewer.pal(9,"Blues"))
+  wordcloud(selected.data2$word, selected.data2$count,
+            main = "2nd Term",
+            scale=c(5,0.5),
+            max.words=70,
+            min.freq=1,
+            random.order=FALSE,
+            rot.per=0.0,
+            use.r.layout=T,
+            random.color=FALSE,
+            colors=brewer.pal(9,"Greens"))
+  # display.brewer.all()
+}
+
+ggplot.flow <-function(corpus, president){
+  speech.flow.1 <- flow(corpus, president, 1)
+  speech.flow.1$term <- "Term 1"
+  speech.flow.2 <- flow(corpus, president, 2)
+  speech.flow.2$term <- "Term 2"
+  
+  speech.flow <- rbind(speech.flow.1, speech.flow.2)
+  speech.flow$colour <- ifelse(speech.flow$score < 0, "negative","positive")
+  
+  ggplot(speech.flow, aes(section, score, fill=colour)) +
+    geom_bar(stat = "identity", show.legend = FALSE) +
+    scale_fill_manual(values=c(positive="steelblue",negative="firebrick1")) +
+    facet_wrap(~ term, nrow = 2) +
+    xlab("Speech") +
+    ylab("") +
+    ggtitle(president)
+}
---
+++ lib/processing.R
@@ -0,0 +1,45 @@
+
+# function update.tables
+# this function takes as an input a tibble and the list of presidents to keep and returns 
+# .. the subset table
+update.tables <- function(arg1, arg2){
+  
+  arg1$document <- substr(arg1$document, 6, nchar(arg1$document)-4)
+  names(arg1)[names(arg1)=="term"] <- "word"
+  arg1$president <- substr(arg1$document, 0, nchar(arg1$document)-2)
+  arg1$term <- substr(arg1$document, nchar(arg1$document), 
+                          nchar(arg1$document))
+  arg1 <- arg1[arg1$president %in% as.list(arg2), ]
+  
+  return(arg1)
+}
+
+# funtion flow
+# this function takes as an input the name of the president and the corpus and return 
+# .. a sentiment score for each section of his speech (the number of 
+# .. sections has eben defined to 20)
+flow <- function(corpus, president, term){
+  # process the text
+  text <- paste0("inaug",president,"-",term,".txt")
+  speech <- data_frame(text = corpus[[text]]$content)
+  speech <- unnest_tokens(speech, word, text)
+  speech$order <- c(1:nrow(speech))
+  nb.words <- nrow(speech)
+  
+  # upload sentiments
+  speech <- inner_join(speech,
+                       get_sentiments("nrc"),
+                       by=c("word"="word"))
+  speech$count <- 1
+  speech <- spread(speech, sentiment, count, fill=0)
+  speech <- mutate(speech, sentiment = positive - negative)
+  speech <- group_by(speech, order, word, sentiment)
+  
+  # compute sections
+  nb.sections = 20
+  speech$section <- floor(speech$order*nb.sections/nb.words)
+  
+  # compute sentiment score per section
+  speech.flow <- summarise(group_by(speech, section), score = sum(sentiment))
+  
+}
